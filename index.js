fa1d74f1e2f09b21184906b69cb903d2fcdd4a3a
Chowdary G <chowdary716@gmail.com>
2021-03-27 00:39:57 +0530

    added connection to socket.io

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
-const dotenv = require("dotenv"),
+const dotenv = require("dotenv").config(),
   Config = require("./src/config/config"),
-  Mongoose = require('mongoose'),
-  Connection=require('./src/config/mongoose-connection');
+  app = require("./src/server"),
+  socket = require("socket.io"),
+  Mongoose = require("mongoose"),
+  Connection = require("./src/config/mongoose-connection"),
+  port = process.env.PORT || Config.deafult.port;
 
-dotenv.config();
+let server = require("http").createServer(app);
 
-Mongoose.Promise = Promise
+Mongoose.Promise = Promise;
 
-Mongoose.connection.on('connected', () => {
-  console.log('Connection Established')
-})
+Mongoose.connection.on("connected", () => {
+  console.log("Connection Established");
 
-Mongoose.connection.on('reconnected', () => {
-  console.log('Connection Reestablished')
-})
+  //INFO: http server
+  server = app.listen(port, function () {
+    console.log(`Server is running on: http://localhost:${port}`);
+  });
 
-Mongoose.connection.on('disconnected', () => {
-  console.log('Connection Disconnected')
-})
+  //INFO: socket connection
+  let io = socket(server);
+  app.set("socketio", io);
+  io.sockets.on("connection", function (socket) {
+    console.log("connected");
+  });
+});
 
-Mongoose.connection.on('close', () => {
-  console.log('Connection Closed')
-})
+Mongoose.connection.on("reconnected", () => {
+  console.log("Connection Reestablished");
+});
 
-Mongoose.connection.on('error', (error) => {
-  console.log('ERROR: ' + error)
-}) 
+Mongoose.connection.on("disconnected", () => {
+  console.log("Connection Disconnected");
+});
+
+Mongoose.connection.on("close", () => {
+  console.log("Connection Closed");
+});
+
+Mongoose.connection.on("error", (error) => {
+  console.log("ERROR: " + error);
+});
 
 Connection();
